02a9c8f8089a75e80576de55d3e96932191a2dd0
VanillaChoc <90957909+VanillaChoc@users.noreply.github.com>
2022-06-06 00:04:38 +0700

    a

---
+++ Web2.1/Web2.1/Controllers/CalcServiceController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web2._1.Controllers
+{
+    public class CalcServiceController : Controller
+    {
+        public IActionResult PassUsingViewBag(Services.CalcService calcService)
+        {
+            int a = calcService.GetInt();
+            int b = calcService.GetInt();
+
+            ViewBag.a = a; ViewBag.b = b;
+            ViewBag.Add = calcService.Add(a, b);
+            ViewBag.Sub = calcService.Sub(a, b);
+            ViewBag.Mult = calcService.Mult(a, b);
+            ViewBag.Div = calcService.Div(a, b);
+
+            return View();
+        }
+
+        public IActionResult PassUsingViewData(Services.CalcService calcService)
+        {
+            int a = calcService.GetInt();
+            int b = calcService.GetInt();
+
+            ViewData["a"] = a; ViewData["b"] = b;
+            ViewData["Add"] = calcService.Add(a, b);
+            ViewData["Sub"] = calcService.Sub(a, b);
+            ViewData["Mult"] = calcService.Mult(a, b);
+            ViewData["Div"] = calcService.Div(a, b);
+
+            return View();
+        }
+
+        public IActionResult PassUsingModel(Services.CalcService calcService)
+        {
+            int a = calcService.GetInt();
+            int b = calcService.GetInt();
+
+            Dictionary<string, string> data = new Dictionary<string, string>();
+
+            data["a"] = a.ToString(); data["b"] = b.ToString();
+            data["Add"] = calcService.Add(a, b);
+            data["Sub"] = calcService.Sub(a, b);
+            data["Mult"] = calcService.Mult(a, b);
+            data["Div"] = calcService.Div(a, b);
+
+            return View(data);
+        }
+
+        public IActionResult AccessServiceDirectly()
+        {
+            return View();
+        }
+    }
+}
---
+++ Web2.1/Web2.1/Services/CalcService.cs
@@ -0,0 +1,25 @@
+﻿namespace Web2._1.Services
+{
+    public static class ServiceProviderExtensions
+    {
+        public static void AddCalcService(this IServiceCollection services)
+        {
+            services.AddTransient<CalcService>();
+        }
+    }
+
+    public class CalcService
+    {
+        static Random random = new Random();
+
+        public int GetInt()
+        {
+            return (random.Next() % 11);
+        }
+
+        public string Add(int a, int b) => (a + b).ToString();
+        public string Sub(int a, int b) => (a - b).ToString();
+        public string Mult(int a, int b) => (a * b).ToString();
+        public string Div(int a, int b) => b != 0 ? (a / b).ToString() : "Infinity";
+    }
+}
